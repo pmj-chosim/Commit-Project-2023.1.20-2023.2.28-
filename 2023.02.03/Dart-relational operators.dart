ed8a2a32674ff8fc8b07f22a6ade30e8eba3bd82
박민진 <114579651+pmj-chosim@users.noreply.github.com>
2023-02-03 22:30:33 +0900

    Add files via upload

---
+++ 2023.02.03/Dart-relational operators.dart
@@ -0,0 +1,17 @@
+void main() {
+  int num = 10;
+  
+  // Relational Operators
+
+	  // [Equal to?]
+	print(num==10);
+	  
+	  // [Not Equal to?]
+	print(num!=10);
+	  
+	  // [Greater(Less) than?]
+	print(num<10);
+	  
+	  // [Greater(Less) or Equal to?]
+  print(num>=9);
+}

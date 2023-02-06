bfe579bc1705431a53aae99b3332a7f3611161d8
박민진 <114579651+pmj-chosim@users.noreply.github.com>
2023-02-06 21:41:32 +0900

    Add files via upload

---
+++ 2023.02.06/Dart-list.dart
@@ -0,0 +1,16 @@
+void main() {
+	// Declare and initialize list variable "l1"
+List l1=[1,2,3,4,'h','e',3.4,true,false];
+  
+  // Declare and initialize list variable "l2"
+List l2=['hello', 'world'];
+  
+	// Print all the elements in the list one by one
+ for(int i=0; i<l1.length;i++){
+   print(l1[i]);
+ }
+  for(int i=0; i<l2.length;i++){
+   print(l2[i]);
+ } 
+  
+}

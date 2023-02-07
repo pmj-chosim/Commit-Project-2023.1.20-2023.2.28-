c01b1654a88e0261b651210c121346d2a7cc4cb2
박민진 <114579651+pmj-chosim@users.noreply.github.com>
2023-02-07 22:31:31 +0900

    Add files via upload

---
+++ 2023.02.07/Dart-map(update, delete).dart
@@ -0,0 +1,24 @@
+void main() {
+	// Set Map
+  
+  Map menu={
+    'hamburger':7,
+    'coke':1,
+    'sandwitch':5
+  };
+  print(menu);
+  // Change coke's Value as 2
+  menu['coke']=2;
+  print(menu);
+  
+	// Declare Map Type Variable 
+  Map shake={'shake':3};
+  menu.addAll(shake);
+	// Add 'shake' to Menu
+  menu.addAll({'salad': 2});
+  print(menu);
+  
+  // Remove 'sandwitch'
+  menu.remove('sandwitch');
+  print(menu);
+}

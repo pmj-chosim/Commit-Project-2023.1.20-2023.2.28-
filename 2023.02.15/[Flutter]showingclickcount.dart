f9dabe5156e7c74e831e93b25108cc14180e9b1e
박민진 <114579651+pmj-chosim@users.noreply.github.com>
2023-02-15 21:01:28 +0900

    Add files via upload

---
+++ 2023.02.15/[Flutter]showingclickcount.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SampleApp(),
+    );
+  }
+}
+
+class SampleApp extends StatefulWidget {
+  @override
+  State<SampleApp> createState() => _SampleAppState();
+}
+
+class _SampleAppState extends State<SampleApp> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sample App'),
+      ),
+      body: Center(
+        child: Text('$count'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            count += 1;
+          });
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}

dd500fe85b595cb0153542f82992b7221a57cca6
박민진 <114579651+pmj-chosim@users.noreply.github.com>
2023-02-25 19:58:38 +0900

    Add files via upload

---
+++ 가위바위보.dart
@@ -0,0 +1,52 @@
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+
+  print('Let\'s play Rock, Paper, Scissors!');
+
+  while (true) {
+    stdout.write('Choose your weapon (rock, paper, or scissors): ');
+    String? playerInput = stdin.readLineSync();
+    if (playerInput == null) {
+      break;
+    }
+
+    String playerWeapon = playerInput.trim().toLowerCase();
+
+    if (playerWeapon != 'rock' && playerWeapon != 'paper' && playerWeapon != 'scissors') {
+      print('Invalid input. Try again.');
+      continue;
+    }
+
+    Random random = Random();
+
+    int computerChoice = random.nextInt(3); // 0 = rock, 1 = paper, 2 = scissors
+    String computerWeapon = '';
+
+    switch (computerChoice) {
+      case 0:
+        computerWeapon = 'rock';
+        break;
+      case 1:
+        computerWeapon = 'paper';
+        break;
+      case 2:
+        computerWeapon = 'scissors';
+        break;
+    }
+
+    print('You chose $playerWeapon. The computer chose $computerWeapon.');
+
+    if (playerWeapon == computerWeapon) {
+      print('It\'s a tie!');
+    } else if ((playerWeapon == 'rock' && computerWeapon == 'scissors') ||
+               (playerWeapon == 'paper' && computerWeapon == 'rock') ||
+               (playerWeapon == 'scissors' && computerWeapon == 'paper')) {
+      print('You win!');
+    } else {
+      print('You lose!');
+    }
+
+  }
+}
